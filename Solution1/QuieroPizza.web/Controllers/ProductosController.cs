684a8fce46593979b17439c8b24971689c501b23
Kevin <Kevin@KEVIN>
2022-02-01 20:48:35 -0600

    Creacion de: Lista de productos en la página index de productos. Navegar entre las páginas Home y Productos.

---
+++ Solution1/QuieroPizza.web/Controllers/ProductosController.cs
@@ -0,0 +1,35 @@
+﻿using QuieroPizza.web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuieroPizza.web.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+            var producto1 = new ProductoModel();
+            producto1.id = 1;
+            producto1.Descripcion = "Pizza 6 Quesos";
+
+            var producto2 = new ProductoModel();
+            producto2.id = 2;
+            producto2.Descripcion = "Pizza Hawaiana";
+
+            var producto3 = new ProductoModel();
+            producto3.id = 3;
+            producto3.Descripcion = "Pizza Supreme";
+
+            var ListadeProductos = new List<ProductoModel>();
+            ListadeProductos.Add(producto1);
+            ListadeProductos.Add(producto2);
+            ListadeProductos.Add(producto3);
+
+            return View(ListadeProductos);
+        }
+    }
+}
